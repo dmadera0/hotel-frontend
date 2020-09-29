df0b32a35649ca714d0401d05027874b1ae67304
Daniel <dmadera0@gmail.com>
2020-09-29 15:05:48 -0600

    added log in feature

--- login/login.js
+++ login/login.js
@@ -1,20 +1,38 @@
-console.log("getsome")
-const guest_url = ('http://localhost:3000/guests')
-const form = document.querySelector('form')
 
-form.addEventListener('submit',createUser)
+const form = document.querySelector('#login-form')
+const guest_url = 'http://localhost:3000/guests'
+
+form.addEventListener('submit', createUser)
 
 function createUser(event){
-    event.prevent.default()
+    event.preventDefault()
 
     const formData = new FormData(event.target)
-    const username = formdata.get('username')
-    const party_size = formdata.get('partysize')
-    const password = formdata.get('password')
+    const username = formData.get('username')
+    const party_size = formData.get('party_size')
+    const password = formData.get('password') 
 
-    fetch(guest_url, {
-        method:'Post',
-        headers
+    const guest ={
+        username,
+        party_size,
+        password
     }
+
+    fetch (guest_url, {
+        method: 'POST',
+        headers: { 
+            "Accept": 'application/json',
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            party_size: party_size,
+            password: password
+        })
+    })
+
+    console.log(username,party_size,password)
+
+ 
 
 }
